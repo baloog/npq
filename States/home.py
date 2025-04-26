471d9000ad643858f1506c1c379d4b70d3b59d02
Vignesh_B-Coding <vignesh1998balaji@gmail.com>
2025-04-26 13:56:44 -0400

    Update home.py

--- States/home.py
+++ States/home.py
@@ -1,1 +1,26 @@
-sdfgsf
+import pygame
+from constants import *
+from player_data import player
+
+def draw_home(screen):
+    screen.fill(TEAL)
+    text = medium_font.render(f"Welcome, {player['name']}!", True, WHITE)
+    screen.blit(text, (SCREEN_WIDTH//2 - text.get_width()//2, 30))
+
+    buttons = {}
+    # Track Meal, Play Game, Get Advice, Save buttons drawn here
+    return buttons
+
+def handle_home_buttons(key):
+    from constants import TRACK_MEAL, MINI_GAME, ADVICE
+    from constants import current_state
+
+    if key == 'track_meal':
+        current_state = TRACK_MEAL
+    elif key == 'play_game':
+        current_state = MINI_GAME
+    elif key == 'advice':
+        current_state = ADVICE
+    elif key == 'save':
+        from save_load import save_game
+        save_game()
