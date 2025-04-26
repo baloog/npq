a6f7ee2ca837914bcbf03ee1bf42842a2b4d7534
Vignesh_B-Coding <vignesh1998balaji@gmail.com>
2025-04-26 13:39:05 -0400

    Update constants.py

--- NutriQuest/constants.py
+++ NutriQuest/constants.py
@@ -1,1 +1,33 @@
-efs
+import pygame
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+TEAL = (0, 128, 128)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+PURPLE = (128, 0, 128)
+
+# Screen
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+
+# Fonts
+pygame.font.init()
+title_font = pygame.font.Font(None, 74)
+medium_font = pygame.font.Font(None, 48)
+small_font = pygame.font.Font(None, 36)
+tiny_font = pygame.font.Font(None, 24)
+
+# Game states
+WELCOME = 0
+CREATE_AVATAR = 1
+HOME = 2
+TRACK_MEAL = 3
+MINI_GAME = 4
+LEVEL_UP = 5
+ADVICE = 6
+
+# Global variable
+current_state = WELCOME
