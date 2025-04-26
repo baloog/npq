96b42a38a372263f915a885de84a63d5f7990312
Vignesh_B-Coding <vignesh1998balaji@gmail.com>
2025-04-26 13:57:26 -0400

    Update save_load.py

--- NutriQuest/save_load.py
+++ NutriQuest/save_load.py
@@ -1,1 +1,16 @@
-fsd
+import json
+from player_data import player
+from datetime import datetime
+
+def save_game():
+    player['last_played'] = datetime.now().strftime("%Y-%m-%d")
+    with open('nutriquest_save.json', 'w') as f:
+        json.dump(player, f)
+
+def load_game():
+    global player
+    try:
+        with open('nutriquest_save.json', 'r') as f:
+            player.update(json.load(f))
+    except FileNotFoundError:
+        pass
